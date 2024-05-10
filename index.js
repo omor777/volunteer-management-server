fc0a99cd3400c2e56021a63c304b1fea97bb9d31
omor777 <omor0843@gmail.com>
2024-05-10 16:11:44 +0600

    connect mongodb and added a post route for add volunteer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,13 +18,48 @@ app.use(
 );
 app.use(cookieParser());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ze9kj8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const volunteerCollection = client
+      .db("volunteerManagementDB")
+      .collection("volunteers");
+
+    // add volunteer to the database
+    app.post("/add-volunteer", async (req, res) => {
+      const volunteer = req.body;
+      console.log(volunteer);
+      const result = await volunteerCollection.insertOne(volunteer);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send({ msg: "Volunteer Management system" });
 });
 
-app.listen(port,()=>{
-    console.log(`server on running port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server on running port ${port}`);
+});
 
 // cookie options
 // const cookieOptions = {
